c20d3a7924c3da3909b6ac525f39138c75032ace
Matt Rossetta <archanglmr@yahoo.com>
2016-06-06 15:06:57 -0700

    Remove demo model references.

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    //models.User.findAll({
-    //    include: [ models.Task ]
-    //}).then(function(users) {
-    //    console.log('index users:');
-    //    console.log(users);
-    //    res.render('index', {
-    //        title: 'Express',
-    //        users: users
-    //    });
-    //});
     res.render('index', { title: 'Express' });
 });
 
