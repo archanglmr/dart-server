1318be1ece5f83a9c613f75776c45ba0729ea85a
Matt Rossetta <archanglmr@yahoo.com>
2016-06-23 20:39:38 -0400

    Restructured communication between display and game client

--- clients/display.jsx
+++ clients/display.jsx
@@ -1,19 +1,31 @@
+'use strict';
+
+// React dependencies
 import React from 'react';
 import {render} from 'react-dom';
 import Display from './components/Display';
+
+
+// Redux dependencies
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import {updateGameState} from './display/actions';
+import rootReducer from './display/reducers';
+
+// Create the Redux store
+var store = createStore(rootReducer);
+
+let unsubscribe = store.subscribe(() => console.log('display:', store.getState()));
 
 
-render(<Display src="/display/game/01" />, document.getElementById('body'));
+
+render(<Display src="/display/game/01" />, document.getElementById('root'));
 
 
-var socket = io(),
-    store;
+var socket = io();
 
 
-window.registerGame = function(gameStore) {
-  "use strict";
-  store = gameStore;
-
+window.registerGame = function(cb) {
+  cb(store);
   store.dispatch(updateGameState({a:'hello world'}));
 };
--- games/01/client/01.jsx
+++ games/01/client/01.jsx
@@ -5,31 +5,22 @@ import React from 'react';
 import {render} from 'react-dom';
 import GameClient from './components/GameClient';
 
-
 // Redux dependencies
-import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 
-// @fixme Move this to a better spot
-import rootReducer from '../../../clients/display/reducers';
 
-// Create the Redux store
-var store = createStore(rootReducer);
+top.window.registerGame((store) => {
+  let unsubscribe = store.subscribe(() => {
+    console.log('iframe:', store.getState());
+  });
 
-let unsubscribe = store.subscribe(() =>
-    console.log('subscribe:', store.getState())
-);
-
-
-// Render the React root component
-render(
-  (
-    <Provider store={store}>
-      <GameClient />
-    </Provider>
-  ),
-  document.getElementById('root')
-);
-
-
-top.window.registerGame(store);
+  // Render the React root component
+  render(
+      (
+          <Provider store={store}>
+            <GameClient />
+          </Provider>
+      ),
+      document.getElementById('root')
+  );
+});
