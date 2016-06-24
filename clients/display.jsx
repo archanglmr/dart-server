0f201d3942ef9655c6dda6d14b73901d9fc79393
Matt Rossetta <archanglmr@yahoo.com>
2016-06-23 20:49:33 -0400

    Minor cleanup and documentation.

--- clients/display.jsx
+++ clients/display.jsx
@@ -8,24 +8,33 @@ import Display from './components/Display';
 
 // Redux dependencies
 import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-import {updateGameState} from './display/actions';
+import {updateGameState, UPDATE_GAME_STATE} from './display/actions';
 import rootReducer from './display/reducers';
 
 // Create the Redux store
 var store = createStore(rootReducer);
+var socket = io();
 
-let unsubscribe = store.subscribe(() => console.log('display:', store.getState()));
-
-
+//let unsubscribe = store.subscribe(() => console.log('display:', store.getState()));
 
 render(<Display src="/display/game/01" />, document.getElementById('root'));
 
 
-var socket = io();
 
 
-window.registerGame = function(cb) {
+socket.on(UPDATE_GAME_STATE, (data) => {
+  updateGameState(data);
+});
+
+
+/**
+ * This function is called by the iframe window once it's loaded in order to
+ * give it access to the redux store. Once the iframe has the store it should
+ * render it's react components with the Provider component.
+ *
+ * @param cb
+ */
+window.registerGame = (cb) => {
   cb(store);
   store.dispatch(updateGameState({a:'hello world'}));
 };
