e4bba169ae439d21573903b55da30ddb47681bc7
Matt Rossetta <archanglmr@yahoo.com>
2016-06-30 21:43:46 -0700

    Moved color variables. Added feature for highlighting cricket marks

--- lib/components/CricketMark/CricketMark.jsx
+++ lib/components/CricketMark/CricketMark.jsx
@@ -4,16 +4,31 @@ import React, {PropTypes} from 'react';
 import './CricketMark.scss';
 
 
-function CricketMark({marks, defaultText = ''}) {
-  var className = ['cricket-mark', 'c' + Math.min(3, marks)];
+function CricketMark({marks, defaultText = '', highlight = 0}) {
+  var maxMarks = Math.min(marks, 3),
+      className = {
+        svg: ['cricket-mark', 'c' + Math.min(3, marks)],
+        mark1: ['c1'],
+        mark2: ['c2'],
+        mark3: ['c3']
+      },
+      highlightFrom = null;
+
+  if (highlight <= maxMarks) {
+    highlightFrom = Math.max(0, maxMarks - highlight);
+  }
+
+  for (let i = highlightFrom; i < maxMarks; i += 1) {
+    className['mark' + (i + 1)].push('highlight');
+  }
 
   return (
-      <svg className={className.join(' ')} version="1.1" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100">
+      <svg className={className.svg.join(' ')} version="1.1" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 100 100" enable-background="new 0 0 100 100">
         <text className="c0" x="50" y="85" textAnchor="middle" fontSize="100">{defaultText}</text>
         <g transform={`rotate(45, 50, 50)`}>
-          <line className="c1" x1="50" x2="50" y1="5" y2="95" />
-          <line className="c2" x1="5" x2="95" y1="50" y2="50" />
-          <circle className="c3" cx="50" cy="50" r="44"></circle>
+          <line className={className.mark1.join(' ')} x1="50" x2="50" y1="5" y2="95" />
+          <line className={className.mark2.join(' ')} x1="5" x2="95" y1="50" y2="50" />
+          <circle className={className.mark3.join(' ')} cx="50" cy="50" r="44"></circle>
         </g>
       </svg>
   );
@@ -21,6 +36,7 @@ function CricketMark({marks, defaultText = ''}) {
 
 CricketMark.propTypes = {
   marks: PropTypes.number.isRequired,
+  highlight: PropTypes.number,
   defaultText: PropTypes.string
 };
 
